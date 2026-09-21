ecc8ba13e81f082d396bcf9eae5a08913396a57c
AleB <alejoburgos987@gmail.com>
2026-09-21 06:21:56 -0300

    Revise comments and formatting in seed_test_data.sql
    Updated comments and formatting in seed_test_data.sql.

--- database/seed_test_data.sql
+++ database/seed_test_data.sql
@@ -1,5 +1,6 @@
-# Version 2.0 ( Actual)
-Aqui dejo la actual base modificada por mi, ya habia realizado otra que dejare en el documento oficial pero esta es la nueva que aun falta un poco pulir...
+* Version 2.0 ( Actual)
+Aqui dejo la actual base modificada por mi, ya habia realizado otra que dejare en el documento oficial pero esta es la nueva que aun falta un poco pulir... <br>
+	
 ```sql
   -- Pa100T — DB de prueba con datos ficticios
 -- Staff: superhéroes Marvel | Pacientes: dibujos animados
