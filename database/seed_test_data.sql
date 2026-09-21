575726f5ce5a03a14080e95fa6936ee774b4fbbf
AleB <alejoburgos987@gmail.com>
2026-09-21 06:20:44 -0300

    Update seed_test_data.sql with version and comments
    Added version information and comments to seed_test_data.sql

--- database/seed_test_data.sql
+++ database/seed_test_data.sql
@@ -1,3 +1,4 @@
+# Version 2.0 ( Actual)
 Aqui dejo la actual base modificada por mi, ya habia realizado otra que dejare en el documento oficial pero esta es la nueva que aun falta un poco pulir...
 
   -- Pa100T — DB de prueba con datos ficticios
